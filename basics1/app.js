5d5436f5c941fa4c8bf5fdd849310b43d7e0fddb
guiavicar <guiavicar777@gmail.com>
2024-08-30 08:32:03 -0300

    atividades basicas1

---
+++ basics1/app.js
@@ -0,0 +1,29 @@
+alert("Hello")
+
+// declaracao de variavel
+//(reserved keyword) (name) (value)
+let nome = "Caio"
+
+let comida
+comida = "Morango"
+
+console.log(nome)
+
+//matematica
+let Num = 10
+console.log(Num/2) // ou outras contas matematicas
+Num++ // contadores
+Num--
+console.log(Num)
+
+//tipo da variavel
+console.log(typeof Num)
+
+//condicionais === (tipo + valor) = (tipo)
+console.log(Num == 10)
+console.log(Num == "10")
+console.log(Num === 10)
+console.log(Num === "10")
+
+// referencia dentro de "" Atencao para as crases!!!
+let mensagem = `Eu tenho ${Num} balas`
